6f9c7cdabbe1f77ce721e19fb323280e7b1ab9c7
SanskarAwasthi <princea1903@gmail.com>
2024-07-19 23:53:52 +0530

    Supply Chain marketplace on ethereum

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import "../styles/globals.css";
 
+//Internal Import 
+import {TrackingProvider} from '../Conetxt/TrackingContext'
+import { NavBar , Footer } from "../Components"
+
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+    <TrackingProvider>
+    <NavBar></NavBar>
+    <Component {...pageProps} />
+    <div id="modal"></div>
+    </TrackingProvider>
+    <Footer></Footer>
+    </>
+  ) 
 }
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,90 @@
-import React from "react";
+import React, {useState, useEffect,useContext} from "react";
 
-const index = () => {
-  return <div>index</div>;
-};
+//Internal Import
+import {
+  Table,
+  Form,
+  Services,
+  Profile,
+  CompleteShipment,
+  GetShipment,
+  StartShipment,
+} from "../Components/index";
+import { TrackingContext } from "../Conetxt/TrackingContext";
+import Modal from 'react-modal';
+
+Modal.setAppElement('#modal');
+
+const index = () =>{
+  const {
+    currentUser,
+    createShipment,
+    getAllShipment,
+    completeShipment,
+    getShipment,
+    startShipment,
+    getShipmentCount,
+  } = useContext(TrackingContext)
+
+  //State Varibales
+  const [createShipmentModel, setCreateShipmentModel] = useState(false);
+  const [openProfile , setOpenProfile] = useState(false);
+  const [startModal, setStartModal] = useState(false);
+  const [completeModal,setCompleteModal] = useState(false);
+  const [getModel,setGetModel] = useState(false);
+
+  //Data State  Variable
+  const [allShipmentsdata,setallShipmentsdata] = useState()
+
+  useEffect(()=>{
+    const getCampaignsData = getAllShipment();
+
+    return async ()=>{
+      const allData = await getCampaignsData;
+      setallShipmentsdata(allData)
+    }
+
+  },[])
+   return (
+    <>
+       <Services
+       setOpenProfile = {setOpenProfile}
+       setCompleteModal = {setCompleteModal}
+       setGetModel= {setGetModel}
+       setStartModal = {setStartModal}
+       />
+       <Table
+       setCreateShipmentModel = {setCreateShipmentModel}
+       allShipmentsdata = {allShipmentsdata}
+       />
+       <Form
+       createShipmentModel = {createShipmentModel}
+       createShipment = {createShipment}
+       setCreateShipmentModel = {setCreateShipmentModel}
+       />
+       <Profile
+       openProfile = {openProfile}
+       setOpenProfile = {setOpenProfile}
+       currentUser  ={currentUser}
+       getShipmentCount = {getShipmentCount}
+       />
+       <CompleteShipment
+       completeModal = {completeModal}
+       setCompleteModal = {setCompleteModal}
+       completeShipment = {completeShipment}
+       />
+       <GetShipment
+       getModel = {getModel}
+       setGetModel = {setGetModel}
+       getShipment = {getShipment}
+       />
+       <StartShipment
+       startModal = {startModal} 
+       setStartModal = {setStartModal}
+       startShipment = {startShipment}
+       />
+    </>
+   )
+}
 
 export default index;
